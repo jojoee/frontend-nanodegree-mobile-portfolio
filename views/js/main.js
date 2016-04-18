f496fd57e3ef09afda4e97b86b154b2f128c96ca
Nathachai Thongniran <inid3a@gmail.com>
2016-04-18 18:59:48 +0700

    Fix FSL

--- views/js/main.js
+++ views/js/main.js
@@ -501,10 +501,11 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
+  var topOffset = document.body.scrollTop;
 
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((topOffset / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
